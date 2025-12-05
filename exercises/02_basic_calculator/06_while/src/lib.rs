105c50bf92fe72205276760ab584820568665a33
Konstantin Ivanin <ivanin.k@gmail.com>
2025-12-05 19:49:17 +0300

    9 exercise
    Signed-off-by: Konstantin Ivanin <ivanin.k@gmail.com>

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,18 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    // todo!()
+    if n == 0 {
+        1
+    } else {
+        let mut res = 1;
+        let mut i = 1;
+        while i <= n {
+            res *= i;
+            i += 1;
+        }
+        res
+    }
 }
 
 #[cfg(test)]
